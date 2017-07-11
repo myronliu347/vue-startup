66ace924f516212c19b0f5725f5968c45822303b
dwqs <461147874@qq.com>
2017-07-11 12:12:56 +0800

    [misc]image minifying

--- template/build/webpack.base.config.js
+++ template/build/webpack.base.config.js
@@ -39,7 +39,7 @@ module.exports = {
                 use: [{
                     loader: 'url-loader',
                     options: {
-                        limit: 10000,
+                        limit: 8192,
                         name: '[name].[ext]?[hash:8]'
                     }
                 }]
@@ -49,7 +49,7 @@ module.exports = {
                 use: [{
                     loader: 'file-loader',
                     options: {
-                        limit: 10000,
+                        limit: 8192,
                         name: '[name].[ext]?[hash:8]'
                     }
                 }]
