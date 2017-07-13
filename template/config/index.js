73f361f0176d73f8372a83c10f4e6aa2973dd362
dwqs <461147874@qq.com>
2017-07-13 15:59:44 +0800

    [misc]更新环境变量

--- template/config/index.js
+++ template/config/index.js
@@ -7,12 +7,18 @@ module.exports =  {
         assetsRoot: path.resolve(__dirname, '../{{path}}'),
         assetsPublicPath: '{{publicPath}}',
         contentBase: path.resolve(__dirname, '../{{path}}'),
-        port: {{port}}
+        port: {{port}},
+        env: {
+            NODE_ENV: "'development'"
+        }
     },
     build:{
         assetsRoot: path.resolve(__dirname, '../{{path}}'),
         assetsPublicPath: '{{publicPath}}',
-        contentBase: path.resolve(__dirname, '../{{path}}')
+        contentBase: path.resolve(__dirname, '../{{path}}'),
+        env: {
+            NODE_ENV: "'production'"
+        }
     },
     prefix: {
         development: '',
